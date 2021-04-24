d41b2209ef52e38c0490822207727d7ccd60c48a
Dmitry <verchyk91@mail.ru>
2021-04-24 17:01:06 +0300

    can_move

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+import pygame
 from logics import *
+import sys
+
+
+def draw_interface():
+    pygame.draw.rect(screen, WHITE, TITLE_REC)
+    font = pygame.font.SysFont("stxingkai", 70)
+    pretty_print(mas)
+    for row in range(BLOCKS):
+        for column in range(BLOCKS):
+            value = mas[row][column]
+            text = font.render(f'{value}', True, BLACK)
+            w = column * SIZE_BLOCK + (column + 1) * MARGIN
+            h = row * SIZE_BLOCK + (row + 1) * MARGIN + SIZE_BLOCK
+            pygame.draw.rect(screen, COLORS[value], (w, h, SIZE_BLOCK, SIZE_BLOCK))
+            if value != 0:
+                font_w, font_h = text.get_size()
+                text_x = w + (SIZE_BLOCK - font_w) / 2
+                text_y = h + (SIZE_BLOCK - font_h) / 2
+                screen.blit(text, (text_x, text_y))
+
 
 mas = [
     [0, 0, 0, 0],
@@ -7,17 +28,59 @@ mas = [
     [0, 0, 0, 0],
 ]
 
+COLORS = {
+    0: (130, 130, 130),
+    2: (255, 255, 255),
+    4: (255, 255, 128),
+    8: (255, 255, 0),
+    16: (255, 235, 255),
+    32: (255, 235, 128),
+    64: (255, 235, 0),
+}
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (130, 130, 130)
+
+BLOCKS = 4
+SIZE_BLOCK = 110
+MARGIN = 10
+WIDTH = SIZE_BLOCK * BLOCKS + MARGIN * (BLOCKS + 1)
+HEIGTH = WIDTH + 110
+TITLE_REC = pygame.Rect(0, 0, WIDTH, 110)
+
 mas[1][2] = 2
 mas[3][0] = 4
 print(get_empty_list(mas))
 pretty_print(mas)
 
-while is_zero_in_mas(mas):
-    input()
-    empty = get_empty_list(mas)
-    random.shuffle(empty)
-    random_num = empty.pop()
-    x, y = get_index_from_number(random_num)
-    mas = insert_2_or_4(mas, x, y)
-    print(f"Мы заполнили элемент под номером {random_num}")
-    pretty_print(mas)
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGTH))
+pygame.display.set_caption("2048")
+draw_interface()
+pygame.display.update()
+
+
+while is_zero_in_mas(mas) or can_move(mas):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                mas = move_left(mas)
+            elif event.key == pygame.K_RIGHT:
+                mas = move_right(mas)
+            elif event.key == pygame.K_UP:
+                mas = move_up(mas)
+            elif event.key == pygame.K_DOWN:
+                mas = move_down(mas)
+            empty = get_empty_list(mas)
+            random.shuffle(empty)
+            random_num = empty.pop()
+            x, y = get_index_from_number(random_num)
+            mas = insert_2_or_4(mas, x, y)
+            print(f"Мы заполнили элемент под номером {random_num}")
+            draw_interface()
+            pygame.display.update()
+
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from logics import get_number_from_index, get_index_from_number, get_empty_list, is_zero_in_mas
+from logics import move_left, move_up, move_down, can_move
 
 
 class Test_2048(unittest.TestCase):
@@ -78,6 +79,86 @@ class Test_2048(unittest.TestCase):
             [1, 1, 0, 1],
         ]
         self.assertEqual(is_zero_in_mas(mas), True)
+
+    def test_12(self):
+        a = []
+        mas = [
+            [2, 2, 0, 0],
+            [0, 4, 4, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        rez = [
+            [4, 0, 0, 0],
+            [8, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        self.assertEqual(move_left(mas), rez)
+
+    def test_13(self):
+        a = []
+        mas = [
+            [2, 4, 4, 2],
+            [4, 0, 0, 2],
+            [0, 0, 0, 0],
+            [8, 8, 4, 4],
+        ]
+        rez = [
+            [2, 8, 2, 0],
+            [4, 2, 0, 0],
+            [0, 0, 0, 0],
+            [16, 8, 0, 0],
+        ]
+        self.assertEqual(move_left(mas), rez)
+
+    def test_14(self):
+        mas = [
+            [2, 4, 0, 2],
+            [2, 0, 2, 0],
+            [4, 0, 2, 4],
+            [4, 4, 0, 0],
+        ]
+        rez = [
+            [4, 8, 4, 2],
+            [8, 0, 0, 4],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        self.assertEqual(move_up(mas), rez)
+
+    def test_15(self):
+        mas = [
+            [2, 4, 0, 2],
+            [2, 0, 2, 0],
+            [4, 0, 2, 4],
+            [4, 4, 0, 0],
+        ]
+        rez = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [4, 0, 0, 2],
+            [8, 8, 4, 4],
+        ]
+        self.assertEqual(move_down(mas), rez)
+
+    def test_16(self):
+        mas = [
+            [2, 4, 0, 2],
+            [2, 0, 2, 0],
+            [4, 0, 2, 4],
+            [4, 4, 0, 0],
+        ]
+        self.assertEqual(can_move(mas), True)
+
+    def test_17(self):
+        mas = [
+            [2, 4, 8, 2],
+            [9, 7, 11, 12],
+            [13, 31, 111, 54],
+            [74, 47, 70, 99],
+        ]
+        self.assertEqual(can_move(mas), False)
 
 
 if __name__ == 'main':
